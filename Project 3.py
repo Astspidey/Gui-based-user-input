4b16250212efab574bdd59a9711918420a518bb2
Astspidey <152585705+Astspidey@users.noreply.github.com>
2024-01-22 20:21:52 +0530

    Add files via upload

---
+++ Project 3.py
@@ -0,0 +1,8 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> python -m pipinstall
+SyntaxError: invalid syntax
+>>> from tkinter import *
+>>> 
+==== RESTART: C:/Users/yash/AppData/Local/Programs/Python/Python312/JIdf0.py ===
+Hello Yashvardhan
