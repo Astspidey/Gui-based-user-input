492814e39362c971412f1dbd020ac7110ab13071
Astspidey <152585705+Astspidey@users.noreply.github.com>
2024-01-22 20:23:09 +0530

    Project 3.py

--- Project 3.py
+++ Project 3.py
@@ -1,8 +1,37 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> python -m pipinstall
-SyntaxError: invalid syntax
->>> from tkinter import *
->>> 
-==== RESTART: C:/Users/yash/AppData/Local/Programs/Python/Python312/JIdf0.py ===
-Hello Yashvardhan
+from tkinter import *
+
+def submit():
+    username = entry.get() #gets entry text
+    print("Hello "+username)
+
+def delete():
+    entry.delete(0,END) #deletes the line of text
+
+def backspace():
+    entry.delete(len(entry.get())-1,END) #delete last character
+
+window = Tk()
+window.title("user input")
+label = Label(window,text="username: ")
+label.config(font=("Consolas",30))
+label.pack(side=LEFT)
+
+submit = Button(window,text="submit",command=submit)
+submit.pack(side = RIGHT)
+
+delete = Button(window,text="delete",command=delete)
+delete.pack(side = RIGHT)
+
+backspace = Button(window,text="backspace",command=backspace)
+backspace.pack(side = RIGHT)
+
+entry = Entry()
+entry.config(font=('Ink Free',50)) #change font
+entry.config(bg='#111111') #background
+entry.config(fg='#00FF00') #foreground
+entry.config(width=10) #width displayed in characters
+#entry.insert(0,'Spongebob') #set default text
+#entry.config(state=DISABLED) #ACTIVE/DISABLED
+#entry.config(show='*') #replace characters shown with x character
+entry.pack()
+window.mainloop()
